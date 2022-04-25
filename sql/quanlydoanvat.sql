b3f6182e212ff93688e9de64caa2b9bca81bf077
NguyenThiThuTrang <99708445+tranggnguyen@users.noreply.github.com>
2022-04-25 19:08:12 +0700

    them sql

---
+++ sql/quanlydoanvat.sql
@@ -0,0 +1,37 @@
+﻿create database quanlydoanvat
+go
+use quanlydoanvat
+create table Menu
+( maDoAn nvarchar(10) not null,
+tenDoAn nvarchar(50) not null,
+loạiDoAn nvarchar(50) not null,
+giaThanh nvarchar(50) not null,
+primary key (maDoAn)
+)
+go
+create table taiKhoan
+(
+taiKhoan nvarchar(100) not null,
+matKhau nvarchar(20) not null,
+hoTen nvarchar(20) not null,
+sdt nvarchar(11) not null,
+chucVu nvarchar(50) not null,
+diaChi nvarchar(30) not null,
+primary key(taiKhoan)
+)
+go
+create table baoCao
+(id int not null IDENTITY(1,1),
+baoCao nvarchar(50) not null,
+phanhoi nvarchar(50) not null,
+primary key(id)
+)
+go
+create table thongKe
+( maHoaDon nvarchar(20) not null,
+ ngayNhapHoaDon nvarchar(20) not null,
+ soTienHoaDon nvarchar(50) not null,
+ primary key(maHoaDon)
+ )
+ 
+
